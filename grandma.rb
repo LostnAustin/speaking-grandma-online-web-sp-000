c65e40427f54794d61f7ed2c63e6cf6a788552de
Chad Lengefeld <chadlengefeld@gmail.com>
2019-10-21 03:58:18 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 # Write a speak_to_grandma method.
 
 def speak_to_grandma(string)
-  if !!string.upcase == ("I LOVE YOU GRANDMA!")
+  if string.upcase == ("I LOVE YOU GRANDMA!")
 
     return "I LOVE YOU TOO PUMPKIN!"
 #  elsif string == "Hi Nana, how are you?"
