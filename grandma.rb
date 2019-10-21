a5fbc548784bea305658e4871d5f9f9365290680
Chad Lengefeld <chadlengefeld@gmail.com>
2019-10-21 04:01:07 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -12,7 +12,7 @@ def speak_to_grandma(string)
     !!string.upcase == "WHAT?"
      puts "NO, NOT SINCE 1938!"
     end
-
+end
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
