7b11472a21bdf6b84cadbafcd914d9e1b3469562
Chad Lengefeld <chadlengefeld@gmail.com>
2019-10-21 04:15:07 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,12 +3,12 @@
 def speak_to_grandma(string)
 if   string.upcase == ("I LOVE YOU GRANDMA!")
     return "I LOVE YOU TOO PUMPKIN!"
-  end
+  
 
-if   string.upcase == "WHAT DID YOU EAT TODAY?"
+elsif   string.upcase == "WHAT DID YOU EAT TODAY?"
     return "NO, NOT SINCE 1938!"
-  end
-if  string.upcase == "WHAT DID YOU EAT TODAY?"
+  
+else  string.upcase == "WHAT DID YOU EAT TODAY?"
      return "NO, NOT SINCE 1938!"
    end
  if  string.upcase != "Hi Nana, how are you?"
