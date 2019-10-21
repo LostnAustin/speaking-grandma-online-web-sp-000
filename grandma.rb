08a9fa5f663a773d975744d85bbf89641f8ccc4c
Chad Lengefeld <chadlengefeld@gmail.com>
2019-10-21 04:17:58 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -7,7 +7,7 @@ if   string.upcase == ("I LOVE YOU GRANDMA!")
   if  string.upcase == "WHAT DID YOU EAT TODAY?"
       return "NO, NOT SINCE 1938!"
     end
-  if  string.upcase == "WHAT DID YOU EAT TODAY?"
+  if  string.upcase == "WHAT?"
        return "NO, NOT SINCE 1938!"
      end
  if  string.upcase != "Hi Nana, how are you?"
